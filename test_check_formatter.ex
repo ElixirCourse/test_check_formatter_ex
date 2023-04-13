ffcc2b4d490832dc0546f5dfa3f24b28eb9ae747
ivan <IvanIvanoff@users.noreply.github.com>
2023-04-13 22:45:02 +0300

    Update test_check_formatter.ex

--- test_check_formatter.ex
+++ test_check_formatter.ex
@@ -4,13 +4,8 @@ defmodule TestCheckFormatter do
 
   ## Callbacks
 
-  def init(hehe) do
-    {:ok,
-     %{
-       fail_tasks: [],
-       success_tasks: [],
-       fail_tests: []
-     }}
+  def init(_) do
+    {:ok, :will_be_setup_in_each_module_started}
   end
 
   defp get_max_points_from_env() do
@@ -20,7 +15,7 @@ defmodule TestCheckFormatter do
     end
   end
 
-  def handle_cast({:module_started, test_module}, state) do
+  def handle_cast({:module_started, test_module}, _state) do
     # Max points will be set as a moduletag attribute in the test module
     # `@moduletag max_points: 15`
     max_points =
@@ -29,7 +24,13 @@ defmodule TestCheckFormatter do
         [test | _] -> test.tags.max_points
       end
 
-    {:noreply, Map.put(state, :max_points, max_points)}
+    {:noreply,
+     %{
+       max_points: max_points,
+       fail_tasks: [],
+       success_tasks: [],
+       fail_tests: []
+     }}
   end
 
   def handle_cast({_, %{tags: %{test_type: :doctest}}}, state) do
@@ -66,8 +67,13 @@ defmodule TestCheckFormatter do
     {:noreply, state}
   end
 
-  def handle_cast({:module_finished, _times_us}, state) do
-    print_suite(state)
+  def handle_cast({:module_finished, test_module}, state) do
+    print_module_finished(state, test_module)
+    {:noreply, state}
+  end
+
+  def handle_cast({:suite_finished, _}, state) do
+    print_suite_finished()
     {:noreply, state}
   end
 
@@ -75,7 +81,7 @@ defmodule TestCheckFormatter do
     {:noreply, state}
   end
 
-  defp print_suite(state) do
+  defp print_module_finished(state, test_module) do
     max_points = state.max_points
 
     total_success_tasks =
@@ -85,6 +91,9 @@ defmodule TestCheckFormatter do
     total_tasks = Enum.uniq(state.success_tasks ++ state.fail_tasks) |> length()
 
     percent_success = percent_of_0_to_1(total_tasks, total_success_tasks)
+    points_earned = Float.round(percent_success * max_points, 2)
+
+    update_process_dictionary(max_points, points_earned)
 
     failed_tests_str =
       Enum.with_index(state.fail_tests, 1)
@@ -93,25 +102,45 @@ defmodule TestCheckFormatter do
       end)
       |> Enum.join("\n")
 
-    failed_tests_str = IO.ANSI.red() <> "Failed tests:\n" <> failed_tests_str <> IO.ANSI.reset()
-
-    IO.puts(failed_tests_str <> "\n")
+    if failed_tests_str != "" do
+      failed_tests_str = IO.ANSI.red() <> "Failed tests:\n" <> failed_tests_str <> IO.ANSI.reset()
+      IO.puts(failed_tests_str <> "\n")
+    end
 
     IO.puts("""
-    Report:
+    Report for test module #{test_module.name}:
       Max points for the current homework: #{max_points}
       Tasks: #{total_tasks}
       Tasks have at least one failed test: #{total_failed_tasks}
       Tasks that don't have failing tests: #{total_success_tasks}
       Percent successful tests: #{percent_success * 100}%
 
-      Points assigned: #{Float.round(percent_success * max_points, 2)}
+      Points assigned: #{points_earned}
     """)
 
     :ok
   end
 
+  defp print_suite_finished() do
+    points_earned = Process.get(:points_earned)
+    max_points = Process.get(:max_points)
+
+    IO.puts("""
+    Report for all test modules:
+      Max points: #{Enum.sum(max_points) * 1.0}
+      Points earned: #{Enum.sum(points_earned)}
+    """)
+  end
+
   defp percent_of_0_to_1(total, part) do
     Float.round(part / total, 2)
+  end
+
+  defp update_process_dictionary(max_points, points_earned) do
+    points = Process.get(:points_earned, [])
+    Process.put(:points_earned, [points_earned | points])
+
+    points = Process.get(:max_points, [])
+    Process.put(:max_points, [max_points | points])
   end
 end
